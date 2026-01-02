653a0b60e0e6b389240144456f566eb355010ff4
spermer <spermer@example.com>
2026-01-02 19:45:48 +0100

    paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/docs/',
+      base: './',
       build: {
         outDir: 'docs',
       },
